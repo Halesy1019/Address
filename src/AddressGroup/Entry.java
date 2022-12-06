38e1a2f341ed6a89fb0014930711da70c532a558
Haley Brightwell <hbrightwell1101@gmail.com>
2022-12-05 19:35:30 -0600

    Add entry class field and constructors

---
+++ src/AddressGroup/Entry.java
@@ -0,0 +1,56 @@
+package AddressGroup;
+
+public class Entry {
+	//fields
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String emailAddress;
+	
+	//creating constructors
+	//method that you call when you want to make a new instance of a thing
+	public Entry() {
+		this("", "", "", "");
+	}
+	
+	//overloaded constructor 
+	public Entry(String firstName, String lastName, String phoneNumber, String emailAddress) {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setPhoneNumber(phoneNumber);
+		setEmailAddress(emailAddress);
+	}
+	
+	
+	//getter and setter method of these main fields above
+	public String getFirstName() {
+		return firstName;
+		
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+  
+  
+  
+ 
+}
